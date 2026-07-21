cdab3e140d05bf3d6ee8dcae9a76f0f72ef64d1a
Rostyslav` <hrostyslav@ukr.net`>
2026-07-21 13:52:32 +0300

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
-  images: string[],
+  images: string[];
   itemWidth: number;
   frameSize: number;
   step: number;
@@ -12,7 +12,6 @@ interface State {
 }
 
 class App extends React.Component<{}, State> {
-
   state = {
     images: [
       './img/1.png',
@@ -34,35 +33,46 @@ class App extends React.Component<{}, State> {
   };
 
   render() {
-    const { images, step, frameSize, itemWidth, animationDuration, infinite } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration, infinite } =
+      this.state;
 
     return (
       <div className="App">
         <h1 data-cy="title">Carousel with {images.length} images</h1>
         <div className="controls">
           <label htmlFor="stepId">Step: </label>
-
           <input
-            id='stepId'
+            id="stepId"
             type="text"
             value={step}
-            onChange={(e) => this.setState({step: +e.target.value || 0})}
+            onChange={e => this.setState({ step: +e.target.value || 0 })}
           />
 
-          <label htmlFor="itemId">Item Size:</label>
-          
-          <input
-            type="text"
-            value={itemWidth}
-            onChange={(e) => this.setState({itemWidth: +e.target.value || 0})}
-            id="itemId" />
-          
-          <label htmlFor="frameId">Frame Size:</label>
-          <input
-            type="text"
-            value={frameSize}
-            onChange={(e) => this.setState({frameSize: +e.target.value || 0})}
-            id="frameId" />
+          <label htmlFor="itemId">Item Size:
+            <input
+              type="text"
+              value={itemWidth}
+              onChange={e => this.setState({ itemWidth: +e.target.value || 0 })}
+              id="itemId"
+            /></label>
+
+
+          <label htmlFor="frameId">
+            Frame Size:
+            <input
+              type="text"
+              value={frameSize}
+              onChange={e => this.setState({ frameSize: +e.target.value || 0 })}
+              id="frameId"
+            /></label>
+
+
+          <label htmlFor="animationId">Animation duration:
+            <input
+              type="text"
+              value={animationDuration}
+              onChange={e => this.setState({ animationDuration: +e.target.value || 0 })} />
+          </label>
         </div>
 
         <Carousel
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,42 +18,58 @@ const Carousel: React.FC<Props> = ({
   animationDuration = 1000,
   infinite = false,
 }) => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div className="Carousel"
-      style={{ width: frameSize * itemWidth }}>
-      <ul className="Carousel__list"
-        style={
-          {
-            transform: `translateX(${-currentIndex * itemWidth}px)`,
-            transition: `transform ${animationDuration}ms`
-          }}>
-        {images.map((image, index) =>
-          <li key={`${image}-${index}`}
-            style={{ width: itemWidth }}>
-            <img data-cy="image" src={image} alt={`image-${index}`} width={itemWidth}/>
+    <div className="Carousel" style={{ width: frameSize * itemWidth }}>
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(${-currentIndex * itemWidth}px)`,
+          transition: `transform ${animationDuration}ms`,
+        }}
+      >
+        {images.map((image, index) => (
+          <li key={`${image}-${index}`} style={{ width: itemWidth }}>
+            <img
+              data-cy="image"
+              src={image}
+              alt={`image-${index}`}
+              width={itemWidth}
+            />
           </li>
-        )}
-      </ul >
+        ))}
+      </ul>
 
       <button
         data-cy="prev"
         onClick={() =>
-          setCurrentIndex(prev => infinite === true
-            && prev === 0 ? images.length - frameSize
-            : Math.max(0, prev - step))}
+          setCurrentIndex(prev =>
+            infinite === true && prev === 0
+              ? images.length - frameSize
+              : Math.max(0, prev - step),
+          )
+        }
         disabled={!infinite && currentIndex === 0}
-        type="button">Prev</button>
+        type="button"
+      >
+        Prev
+      </button>
       <button
         data-cy="next"
-        onClick={() => setCurrentIndex(next => infinite === true
-          && next >= images.length - frameSize
-          ? 0 : Math.min(images.length - frameSize, next + step))}
+        onClick={() =>
+          setCurrentIndex(next =>
+            infinite === true && next >= images.length - frameSize
+              ? 0
+              : Math.min(images.length - frameSize, next + step),
+          )
+        }
         disabled={!infinite && currentIndex >= images.length - frameSize}
-        type="button">Next</button>
-    </div >
+        type="button"
+      >
+        Next
+      </button>
+    </div>
   );
 };
 
